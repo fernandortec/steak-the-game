0d6da10e83bf2bef093ec188774937da950a12a1
Cotemig <cotemig@IMAC14.local>
2021-11-23 11:24:56 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import steakImg from './assets/steak.png'
+
+const App = () => {
+  const [isGameInitiated, setIsGameInitiated] = useState<boolean>(false)
+  const [timeOut, setTimeOut] = useState<number>(500)
+  const [mouseOverCount, setMouseOverCount] = useState<number>(0)
+  const [imageXandY, setImageXAndY] = useState<number[]>([50, 50])
+  const [timesClicked, setTimesCLicked] = useState<number>(0)
+
+  const raiseScore = (): void => {
+    setTimesCLicked(timesClicked  +  1)
+
+    setTimeOut(timeOut > 0 ? timeOut - 50 : timeOut)
+  }
+
+  return(
+    <div className="container">
+      <h2>Welcome to steak the game</h2>
+      <p>Press down for the game to start</p>
+      
+    <p>Mouse over count: {mouseOverCount}</p>
+    <p>Clicked counts: {timesClicked}</p>
+      <div className="steak-box">
+        <img src={steakImg} style={{
+          display: isGameInitiated ? 'block' : 'none',
+          top: imageXandY[0],
+          left: imageXandY[1]
+        }} onMouseOver={() => {
+          setTimeout(() => {
+            setMouseOverCount(mouseOverCount + 1)
+            setImageXAndY([Math.floor(Math.random() * (500 + 1)),Math.floor(Math.random() * (500 + 1))])
+          }, timeOut);
+        }}
+        onClick={()  => raiseScore()}
+        />
+      </div>
+
+      <button onClick={() => setIsGameInitiated(true)}>Start</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export { App }
